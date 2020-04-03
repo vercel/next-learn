2a4e1b13e65fe6367bf4ce4b4c796a2e25dd2c9f
Shu Uesugi <shu@chibicode.com>
2020-04-02 20:21:12 -0700

    Add API Routes starter

--- api-routes-starter/pages/index.js
+++ api-routes-starter/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/Layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/Date'
 
 const Home = ({ allPostsData }) => (
   <Layout home>
@@ -9,18 +11,30 @@ const Home = ({ allPostsData }) => (
       <title>{siteTitle}</title>
     </Head>
     <section className={utilStyles.headingMd}>
-      <p>[Your Self Introduction]</p>
+      <p>
+        Hello, I’m <strong>Shu</strong>. I write code at{' '}
+        <a href="https://zeit.co">ZEIT</a>, the team behind{' '}
+        <a href="https://nextjs.org/">Next.js</a>. You can contact me via{' '}
+        <a href="https://twitter.com/chibicode">Twitter</a> or{' '}
+        <a href="mailto:chibicode@zeit.co">email</a>.
+      </p>
+      <p>
+        (This is a sample website - you’ll be building a site like this on{' '}
+        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+      </p>
     </section>
     <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
       <h2 className={utilStyles.headingLg}>Blog</h2>
       <ul className={utilStyles.list}>
         {allPostsData.map(({ id, date, title }) => (
           <li className={utilStyles.listItem}>
-            {title}
+            <Link href="/posts/[id]" as={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
             <br />
-            {id}
-            <br />
-            {date}
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
           </li>
         ))}
       </ul>
@@ -32,8 +46,8 @@ export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
     props: {
-      allPostsData,
-    },
+      allPostsData
+    }
   }
 }
 
