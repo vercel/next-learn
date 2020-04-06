0f3f964494f9d594eb468a4a3ebdb70bd43a07ac
Shu Uesugi <shu@chibicode.com>
2020-04-05 20:59:04 -0700

    Page -> Layout

--- api-routes-starter/components/Layout.js
+++ api-routes-starter/components/Layout.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const name = '[Your Name]'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Page({ children, home }) {
+export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
