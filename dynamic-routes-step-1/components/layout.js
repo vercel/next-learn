8ea5352da90ec7a6c62629f300d56b90520c4240
Shu Uesugi <shu@chibicode.com>
2020-04-20 20:01:21 -0700

    Add twitter card required attributes

--- dynamic-routes-step-1/components/layout.js
+++ dynamic-routes-step-1/components/layout.js
@@ -21,6 +21,8 @@ export default function Layout({ children, home }) {
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
