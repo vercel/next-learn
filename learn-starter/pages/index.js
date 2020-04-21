64e2f6d52b037d8aae31bbb937e84d1a0fe80747
Shu Uesugi <shu@chibicode.com>
2020-04-21 08:23:14 -0700

    Update create-next-app templates

--- learn-starter/pages/index.js
+++ learn-starter/pages/index.js
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-
 import Head from 'next/head'
 
 export default function Home() {
@@ -12,10 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Read{' '}
-          <Link href="/posts/first-post">
-            <a>this page!</a>
-          </Link>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
