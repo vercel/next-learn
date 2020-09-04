4ad9b88616fb09418577a8f0351a0d159dc1c87f
tobudim <tobudim@mdp>
2020-09-04 07:50:09 +0200

    fix Basic finals : date sorting failed to sort + fix #10

--- basics-final/lib/posts.js
+++ basics-final/lib/posts.js
@@ -28,11 +28,7 @@ export function getSortedPostsData() {
   })
   // Sort posts by date
   return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1
-    } else {
-      return -1
-    }
+    return new Date(b.date) - new Date(a.date)
   })
 }
 
