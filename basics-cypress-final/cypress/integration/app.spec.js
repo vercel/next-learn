0786b819f65a58ab5dd17702143e223416e91979
Kevin Old <kevin@cypress.io>
2020-09-08 14:49:50 -0500

    prettier

--- basics-cypress-final/cypress/integration/app.spec.js
+++ basics-cypress-final/cypress/integration/app.spec.js
@@ -1,41 +1,50 @@
-const matter = require('gray-matter');
+const matter = require('gray-matter')
 const { parseISO, format } = require('date-fns')
 
-describe('App', function() {
-    beforeEach(function() {
-        cy.visit("/")
+describe('App', function () {
+  beforeEach(function () {
+    cy.visit('/')
+  })
+
+  it('displays header on home page', function () {
+    cy.get('h1').should('contain', '[Your Name]')
+  })
+
+  it('renders a list of posts', function () {
+    // Testing Tip:
+    // Calculate values vs hard coding numbers.  This makes tests more resilient and adaptive to change.
+    // cy.task("getPostCount") calculates the post count in Node
+    // Learn more about Cypress Tasks: https://on.cypress.io/task
+    cy.task('getPostCount').then(count => {
+      cy.get('[data-test*=post]').should('have.length', count)
     })
+  })
 
-    it('displays header on home page', function() {
-        cy.get('h1').should("contain", "[Your Name]")
-    })
+  it('navigates to a post and verifies header, date and title meta tag', function () {
+    cy.get('[data-test*=post]')
+      .first()
+      .then(post => {
+        cy.get(post)
+          .click()
+          .invoke('attr', 'href')
+          .then(href => {
+            cy.location('pathname').should('equal', href)
 
-    it('renders a list of posts', function() {
-        // Testing Tip:
-        // Calculate values vs hard coding numbers.  This makes tests more resilient and adaptive to change.
-        // cy.task("getPostCount") calculates the post count in Node
-        // Learn more about Cypress Tasks: https://on.cypress.io/task
-        cy.task('getPostCount').then(count => {
-            cy.get('[data-test*=post]').should("have.length", count)
-        })
-    })
+            cy.readFile(`.${href}.md`).then(markdown => {
+              const postMetadata = matter(markdown)
+              const { title, date } = postMetadata.data
+              const formattedDate = format(parseISO(date), 'LLLL d, yyyy')
 
-    it('navigates to a post and verifies header, date and title meta tag', function() {
-        cy.get('[data-test*=post]').first().then(post => {
-            cy.get(post).click().invoke('attr', 'href').then(href => {
-                cy.location("pathname").should("equal", href)
+              cy.get('[data-test=post-title]').should('contain', title)
+              cy.get('time').should('have.text', formattedDate)
 
-                cy.readFile(`.${href}.md`).then(markdown => {
-                    const postMetadata = matter(markdown)
-                    const { title, date} = postMetadata.data
-                    const formattedDate = format(parseISO(date), 'LLLL d, yyyy')
-
-                    cy.get('[data-test=post-title]').should('contain', title)
-                    cy.get('time').should('have.text', formattedDate)
-
-                    cy.get("meta[property='og:title']").should('has.attr', 'content', title)
-                })
+              cy.get("meta[property='og:title']").should(
+                'has.attr',
+                'content',
+                title
+              )
             })
-        })
-    })
+          })
+      })
+  })
 })
