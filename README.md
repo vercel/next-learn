18d3aab3efff4e48eaeae5876a927bf21d5d9d90
Lee Robinson <lrobinson2011@gmail.com>
2021-01-11 16:51:36 -0600

    Use next/image and image optimization.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This repository contains starter templates for [Learn Next.js](https://nextjs.org/learn).
 
-The final result for the basics lesson can be found in the [demo](demo) directory and is available at: [https://next-learn-starter.now.sh/](https://next-learn-starter.now.sh/).
+The final result for the basics lesson can be found in the [demo](demo) directory and is available at: [https://next-learn-starter.vercel.app/](https://next-learn-starter.vercel.app/).
--- demo/components/layout.js
+++ demo/components/layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
@@ -17,7 +18,7 @@ export default function Layout({ children, home }) {
         />
         <meta
           property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
+          content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
@@ -27,9 +28,12 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <img
+            <Image
+              priority
               src="/images/profile.jpg"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
@@ -38,9 +42,12 @@ export default function Layout({ children, home }) {
           <>
             <Link href="/">
               <a>
-                <img
+                <Image
+                  priority
                   src="/images/profile.jpg"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
                   alt={name}
                 />
               </a>
