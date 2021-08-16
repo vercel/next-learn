bd85183160158502ff5948da175899dff4bfca55
P Venkat Sai <60128586+venkatsai6@users.noreply.github.com>
2021-08-16 15:35:50 +0530

    Update hello.ts
    Modified '_' to 'req' object in export function and modified text for clear understanding to beginners.

--- typescript-final/pages/api/hello.ts
+++ typescript-final/pages/api/hello.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ text: 'Hello' })
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ text: 'Hello! The blog app is now powered by TypeScript.' })
 }
