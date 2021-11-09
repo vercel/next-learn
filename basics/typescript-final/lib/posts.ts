3a1387166d4aa19a72a377fa379759d3eaf10550
Lee Robinson <me@leerob.io>
2021-11-09 10:59:56 -0600

    Update all dependencies. (#56)

--- basics/typescript-final/lib/posts.ts
+++ basics/typescript-final/lib/posts.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import remark from 'remark'
+import { remark } from 'remark'
 import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
