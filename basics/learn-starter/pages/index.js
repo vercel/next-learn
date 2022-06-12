b6f4b2cd4fe9d7749549273fb52b509134cc8d41
terauchi-yuto <105848119+terauchi-yuto@users.noreply.github.com>
2022-06-12 21:53:25 +0900

    update

--- basics/learn-starter/pages/index.js
+++ basics/learn-starter/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
