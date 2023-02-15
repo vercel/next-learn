e119fa333747f3d4e3e3d7f4b4c3d704e601b847
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-02-15 11:45:06 +0900

    Fix some not unified expressions

--- basics/learn-starter/pages/index.js
+++ basics/learn-starter/pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
