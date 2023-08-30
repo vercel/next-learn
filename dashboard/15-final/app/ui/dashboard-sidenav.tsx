b104775bac395a9547f0e82d50a310570e3afbe8
StephDietz <steph.dietz@vercel.com>
2023-08-30 13:44:20 -0500

    make nav mobile friendly

--- dashboard/15-final/app/ui/dashboard-sidenav.tsx
+++ dashboard/15-final/app/ui/dashboard-sidenav.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { UserGroupIcon, HomeIcon, InboxIcon, CogIcon } from '@heroicons/react/24/solid';
+import { UserGroupIcon, HomeIcon, InboxIcon, PowerIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
@@ -16,9 +16,9 @@ export default function SideNav() {
 	];
 
 	return (
-		<div className="flex flex-col p-4 w-10 md:w-64 border-r  h-full">
+		<div className="flex flex-col p-1 md:p-4 w-12 md:w-64 border-r h-full">
 			<Link href="/">
-				<Image priority src="/logo.svg" height={80} width={80} alt="Logo" className="mb-6" />
+				<Image priority src="/logo.svg" height={80} width={80} alt="Logo" className="mb-4 md:mb-6" />
 			</Link>
 			{tabs.map((tab, i) => {
 				const TabIcon = tab.icon;
@@ -27,20 +27,20 @@ export default function SideNav() {
 						key={i}
 						href={tab.href}
 						className={`mb-2 ${
-							pathname === tab.href ? 'bg-gray-100 text-indigo-600' : ' text-gray-600'
-						} hover:text-indigo-600 hover:bg-gray-100 flex rounded p-2 text-sm font-semibold`}
+							pathname === tab.href ? 'bg-gray-100 text-blue-600' : ' text-gray-600'
+						} hover:text-blue-600 hover:bg-gray-100 flex rounded p-2 text-sm font-semibold`}
 					>
-						<TabIcon className="h-5 w-5 mr-2" />
-						<div>{tab.name}</div>
+						<TabIcon className="h-5 w-5 md:mr-2" />
+						<div className="hidden md:block">{tab.name}</div>
 					</Link>
 				);
 			})}
 			<Link
-				href="/settings"
-				className="flex mt-auto hover:text-indigo-600 hover:bg-gray-100 rounded p-2 text-sm font-semibold"
+				href="/login"
+				className="flex mt-auto hover:text-blue-600 rounded p-2 text-sm font-semibold"
 			>
-				<CogIcon className="h-5 w-5 mr-2" />
-				<div>Settings</div>
+				<PowerIcon className="h-5 w-5 md:mr-2" />
+				<div className="hidden md:block">Sign Out</div>
 			</Link>
 		</div>
 	);
