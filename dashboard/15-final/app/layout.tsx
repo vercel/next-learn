2dd7f149c562c85d4f1723b070d34810898f4847
Delba de Oliveira <delbabrown@gmail.com>
2023-08-30 15:31:32 +0100

    Create hero.tsx

--- dashboard/15-final/app/layout.tsx
+++ dashboard/15-final/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-white ${inter.className}`}>{children}</body>
     </html>
   )
 }
