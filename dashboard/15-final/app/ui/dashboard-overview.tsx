0997278f2ab6feb0b088a22f94298e4b02c3c520
Delba de Oliveira <delbabrown@gmail.com>
2023-08-30 19:17:24 +0100

    Create dashboard-overview.tsx

---
+++ dashboard/15-final/app/ui/dashboard-overview.tsx
@@ -0,0 +1,19 @@
+import Card from "@/app/ui/card";
+import { invoices, customers } from "@/app/lib/dummy-data";
+import { calculateInvoices } from "@/app/lib/calculations";
+
+export default function DashboardOverview() {
+  const totalPaidInvoices = calculateInvoices(invoices, "paid");
+  const totalPendingInvoices = calculateInvoices(invoices, "pending");
+  const numberOfInvoices = invoices.length;
+  const numberOfCustomers = customers.length;
+
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
+      <Card title="Collected" value={totalPaidInvoices} color="green" />
+      <Card title="Pending" value={totalPendingInvoices} color="red" />
+      <Card title="Total Invoices" value={numberOfInvoices} />
+      <Card title="Total Customers" value={numberOfCustomers} />
+    </div>
+  );
+}
