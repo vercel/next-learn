4376abb8219310d5289a82484903b0779b6e689c
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2023-08-30 16:47:42 +0100

    Add components for home and login pages (#128)
    * Run create-next-app
    
    * Add READMEs
    
    * Remove stuff we don't need
    
    * Add dummy data
    
    * Add types for dummy data
    
    * Add dummy routes
    
    * Remove unused CSS
    
    * Split dummy data and definitions
    
    * Add prettier plugin for tailwind
    
    * Create background-blur.tsx
    
    * Create hero.tsx
    
    * Create login-form.tsx
    
    * Tweak
    
    * Fix ts error

---
+++ dashboard/15-final/app/ui/login-form.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import BackgroundBlur from "@/app/ui/background-blur";
+import React, { useState } from "react";
+
+// This component contains basic logic for a React Form.
+// We'll be updating it in Chapter 8 - Adding Authentication.
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <div className="mt-40 flex h-screen flex-col items-center space-y-6">
+      <BackgroundBlur />
+      <a href="/">
+        <img className="h-6 w-auto" src="/logo.svg" alt="Next.js Logo" />
+      </a>
+      <p className="text-center font-semibold text-zinc-900">
+        Log in to your dashboard
+      </p>
+      <div className="w-full max-w-sm">
+        <form onSubmit={handleSubmit} className="px-4">
+          <div>
+            <label
+              className="block text-sm font-medium leading-8 text-zinc-900"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="block w-full rounded-md border-0 p-1.5 text-zinc-900 shadow-sm ring-1 ring-inset ring-zinc-300 sm:text-sm"
+              id="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mt-4">
+            <label
+              className="block text-sm font-medium leading-8 text-zinc-900"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="block w-full rounded-md border-0 p-1.5 text-zinc-900 shadow-sm ring-1 ring-inset ring-zinc-300 sm:text-sm"
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mt-6">
+            <button
+              className="w-full rounded-md bg-black px-4 py-2 text-center text-sm font-semibold text-white hover:bg-zinc-700"
+              type="submit"
+            >
+              Log in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
