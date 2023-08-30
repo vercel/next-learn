9a995933a234ef489742b291b21beffc7aabe520
Stephanie Dietz <49788645+StephDietz@users.noreply.github.com>
2023-08-30 13:06:07 -0500

    Update dashboard/15-final/app/dashboard/layout.tsx
    Co-authored-by: Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>

--- dashboard/15-final/app/dashboard/layout.tsx
+++ dashboard/15-final/app/dashboard/layout.tsx
@@ -4,7 +4,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="flex h-screen">
 			<SideNav />
-			<div>Dashboard layout</div>
 			<div>{children}</div>
 		</div>
 	);
