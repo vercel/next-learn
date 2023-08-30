9fe101702e0e54ac366cf013a4d30b33fe18a6e7
StephDietz <steph.dietz@vercel.com>
2023-08-30 14:42:37 -0500

    update table header and link

--- dashboard/15-final/app/ui/table.tsx
+++ dashboard/15-final/app/ui/table.tsx
@@ -3,19 +3,15 @@ import Link from "next/link";
 
 export default function Example() {
   return (
-    <div className="">
-      <div className="">
-        <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">Invoices</h1>
-        </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button
-            type="button"
-            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Add Invoice
-          </button>
-        </div>
+    <div className="w-full">
+      <div className="w-full flex items-center justify-between">
+        <h1 className="text-base font-semibold leading-6 text-gray-900">Invoices</h1>
+        <Link
+          href="/dashboard/invoices/create"
+          className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Add Invoice
+        </Link>
       </div>
       <div className="mt-8 ">
         <div className="overflow-x-auto">
