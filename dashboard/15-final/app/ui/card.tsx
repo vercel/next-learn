10f0dffc177853c743f91863c6877514524a704f
Delba de Oliveira <delbabrown@gmail.com>
2023-08-31 13:25:12 +0100

    ugh, fonts are hard

--- dashboard/15-final/app/ui/card.tsx
+++ dashboard/15-final/app/ui/card.tsx
@@ -25,10 +25,11 @@ export default function Card({
   const Icon = iconMap[type];
 
   return (
-    <div className="flex justify-between rounded-lg border bg-white p-6 shadow-sm">
+    <div className="flex justify-between rounded-xl border bg-white p-6 shadow-sm">
       <div>
-        <h3 className="text-sm font-semibold text-zinc-400">{title}</h3>
-        <p className="mt-3 text-3xl font-semibold">{value}</p>
+        <h3 className="text-sm font-medium">{title}</h3>
+        <p className="mt-2 text-3xl font-semibold tracking-wide">{value}</p>
+        <p className="mt-1.5 text-sm text-zinc-400">+00% since last month</p>
       </div>
       {Icon ? (
         <Icon className="h-5 w-5 text-zinc-700" aria-label={type} />
