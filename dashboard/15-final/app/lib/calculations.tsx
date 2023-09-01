8bfd6508063d948cf74c61ede5f313fc052335cc
Delba de Oliveira <delbabrown@gmail.com>
2023-09-01 13:33:33 +0100

    Remove unused code

--- dashboard/15-final/app/lib/calculations.tsx
+++ dashboard/15-final/app/lib/calculations.tsx
@@ -13,29 +13,15 @@ export const calculateInvoices = (
     });
 };
 
-export const generateYAxis = (data: Revenue[]) => {
   // Calculate what labels we need to display on the y-axis
   // based on highest record and in 1000s
   const yAxisLabels = [];
-  const highestRecord = Math.max(...data.map((month) => month.revenue));
+  const highestRecord = Math.max(...revenue.map((month) => month.revenue));
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
 
   for (let i = topLabel; i >= 0; i -= 1000) {
     yAxisLabels.push(`$${i / 1000}K`);
   }
 
-  console.log(yAxisLabels);
-
   return { yAxisLabels, topLabel };
-};
-
-export const calculateBarHeight = (revenue: Revenue[]) => {};
-
-export const generateXAxis = (revenue: Revenue[]) => {
-  const xAxisLabels = revenue.map((month) => month.month);
-  const xAxisWidth = xAxisLabels.length * 100;
-
-  const barHeight = revenue.map((month) => month.revenue / 100);
-
-  return { xAxisLabels, xAxisWidth, barHeight };
 };
