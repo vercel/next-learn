4a15f47ad62dfb9cba8d7ff70e5de5c08d08dbc9
StephDietz <steph.dietz@vercel.com>
2023-09-01 12:15:19 -0500

    fix layout overflow issue with table

--- dashboard/15-final/app/dashboard/layout.tsx
+++ dashboard/15-final/app/dashboard/layout.tsx
@@ -4,8 +4,10 @@ import SideNav from "../ui/dashboard-sidenav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen">
-      <SideNav />
-      <div className="w-full">
+      <div className="w-1/12 md:w-1/4 lg:w-1/5">
+        <SideNav />
+      </div>
+      <div className="w-11/12 md:w-3/4 lg:w-4/5">
         <TopNav />
         <div className="p-4 sm:p-10 md:p-20">{children}</div>
       </div>
