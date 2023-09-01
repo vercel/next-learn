fa5258ed804d4d32cbb81157c141b62411fcbb64
Stephanie Dietz <49788645+StephDietz@users.noreply.github.com>
2023-09-01 06:15:38 -0500

    create search bar component (#134)

--- dashboard/15-final/app/ui/dashboard-topnav.tsx
+++ dashboard/15-final/app/ui/dashboard-topnav.tsx
@@ -1,3 +1,9 @@
+import Search from "./search";
+
 export default function TopNav() {
-  return <div className="h-16 border-b">Search</div>;
+  return (
+    <div className="h-16 border-b flex items-center">
+      <Search />
+    </div>
+  )
 }
