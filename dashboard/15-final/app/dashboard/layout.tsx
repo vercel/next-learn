e5bcde9cbb414c07d590e201b47dafacc72cf674
StephDietz <steph.dietz@vercel.com>
2023-09-01 12:45:43 -0500

    make only content portion scrollable

--- dashboard/15-final/app/dashboard/layout.tsx
+++ dashboard/15-final/app/dashboard/layout.tsx
@@ -3,11 +3,11 @@ import SideNav from "../ui/dashboard-sidenav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen">
-      <div className="w-1/12 md:w-1/4 lg:w-1/5">
+    <div className="flex h-screen overflow-hidden">
+      <div className="w-10 md:w-64">
         <SideNav />
       </div>
-      <div className="w-11/12 md:w-3/4 lg:w-4/5">
+      <div className="flex-grow overflow-y-auto">
         <TopNav />
         <div className="p-4 sm:p-10 md:p-20">{children}</div>
       </div>
