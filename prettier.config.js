b419386f8ac6846db9b0b90abdb1aca816e36ba6
Michael Novotny <manovotny@gmail.com>
2023-09-06 12:00:03 -0500

    Match Learn course's prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,6 @@
+const styleguide = require('@vercel/style-guide/prettier');
+
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  ...styleguide,
+  plugins: [...styleguide.plugins, "prettier-plugin-tailwindcss"],
 };
