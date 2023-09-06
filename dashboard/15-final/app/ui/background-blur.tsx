ed9db1aba7773af329fe44d2b0bc383437a85496
Delba de Oliveira <delbabrown@gmail.com>
2023-09-06 16:58:51 +0100

    Polish login form

--- dashboard/15-final/app/ui/background-blur.tsx
+++ dashboard/15-final/app/ui/background-blur.tsx
@@ -1,8 +1,8 @@
 export default function BackgroundBlur() {
   return (
-    <div className="-z-10">
-      <div className="absolute left-[55%] top-[10%] h-40 w-40 rounded-full bg-gradient-to-r from-blue-200 via-blue-300 to-blue-500 opacity-90 blur-3xl"></div>
-      <div className="absolute left-[50%] top-[18%] h-40 w-40 transform rounded-full bg-gradient-to-r from-blue-200 via-blue-300 to-blue-500 opacity-60 blur-3xl"></div>
-    </div>
+    <>
+      <div className="absolute left-[55%] top-[10%] -z-10 h-40 w-40 rounded-full bg-gradient-to-r from-blue-200 via-blue-300 to-blue-500 opacity-90 blur-3xl"></div>
+      <div className="absolute left-[50%] top-[18%] -z-10 h-40 w-40 transform rounded-full bg-gradient-to-r from-blue-200 via-blue-300 to-blue-500 opacity-60 blur-3xl"></div>
+    </>
   );
 }
--- dashboard/15-final/app/ui/login-form.tsx
+++ dashboard/15-final/app/ui/login-form.tsx
@@ -16,55 +16,58 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="mt-40 flex h-screen flex-col items-center space-y-6">
+    <div className="relative mx-auto mt-40 p-4">
       <BackgroundBlur />
-      <a href="/">
-        <img className="h-6 w-auto" src="/logo.svg" alt="Next.js Logo" />
-      </a>
-      <p className="text-center font-semibold text-gray-900">
-        Log in to your dashboard
-      </p>
-      <div className="w-full max-w-sm">
-        <form onSubmit={handleSubmit} className="px-4">
-          <div>
-            <label
-              className="block text-sm font-medium leading-8 text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:text-sm"
-              id="email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mt-4">
-            <label
-              className="block text-sm font-medium leading-8 text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:text-sm"
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="mt-6">
-            <button
-              className="w-full rounded-md bg-black px-4 py-2 text-center text-sm font-semibold text-white hover:bg-gray-700"
-              type="submit"
-            >
-              Log in
-            </button>
-          </div>
-        </form>
+
+      <div className="mx-auto flex max-w-sm flex-col items-center space-y-6 rounded-xl border bg-white px-12 py-12 shadow-sm">
+        <a href="/">
+          <img className="h-6" src="/logo.svg" alt="Next.js Logo" />
+        </a>
+        <p className="text-sm font-semibold text-gray-900">
+          Log in to your dashboard
+        </p>
+        <div className="w-full">
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label
+                className="block text-sm font-medium leading-8 text-gray-900"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:text-sm"
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mt-4">
+              <label
+                className="block text-sm font-medium leading-8 text-gray-900"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:text-sm"
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="mt-8">
+              <button
+                className="w-full rounded-md bg-black py-2 text-center text-sm font-semibold text-white transition-colors hover:bg-gray-800"
+                type="submit"
+              >
+                Log in
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
