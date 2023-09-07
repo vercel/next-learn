cba355b35af4131ee4613ea80a04dea7c50beb06
Delba de Oliveira <delbabrown@gmail.com>
2023-09-07 13:32:02 +0100

    Fix table UI bug

--- dashboard/15-final/app/ui/invoices/table.tsx
+++ dashboard/15-final/app/ui/invoices/table.tsx
@@ -55,83 +55,88 @@ export default function InvoicesTable() {
           Add Invoice
         </Link>
       </div>
-      <div className="mt-8">
+      <div className="mt-8 flow-root">
         <div className="overflow-x-auto">
-          <div className="rounded-md border">
-            <table className="min-w-full divide-y divide-gray-300">
-              <thead className="bg-gray-50 text-left text-sm">
-                <tr>
-                  <th scope="col" className="px-3.5 py-3.5  sm:pl-6">
-                    #
-                  </th>
-                  <th scope="col" className="px-3 py-3.5 font-semibold">
-                    Customer
-                  </th>
-                  <th scope="col" className="px-3 py-3.5 font-semibold">
-                    Email
-                  </th>
-                  <th scope="col" className="px-3 py-3.5 font-semibold">
-                    Amount
-                  </th>
-                  <th scope="col" className="px-3 py-3.5 font-semibold">
-                    Date
-                  </th>
-                  <th scope="col" className="px-3 py-3.5 font-semibold">
-                    Status
-                  </th>
+          <div className="inline-block min-w-full align-middle">
+            <div className="overflow-hidden rounded-md border">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50 text-left text-sm">
+                  <tr>
+                    <th scope="col" className="px-3.5 py-3.5  sm:pl-6">
+                      #
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 font-semibold">
+                      Customer
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 font-semibold">
+                      Email
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 font-semibold">
+                      Amount
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 font-semibold">
+                      Date
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 font-semibold">
+                      Status
+                    </th>
 
-                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                    <span className="sr-only">Edit</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200 text-gray-500">
-                {invoices.map((invoice) => (
-                  <tr key={invoice.id}>
-                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-black sm:pl-6">
-                      {invoice.id}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm">
-                      <div className="flex items-center gap-3">
-                        <Image
-                          src={`${getCustomerById(invoice.customerId)
-                            ?.imageUrl}`}
-                          className="rounded-full"
-                          alt="Customer Image"
-                          width={28}
-                          height={28}
-                        />
-                        <p>{getCustomerById(invoice.customerId)?.name}</p>
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm">
-                      {getCustomerById(invoice.customerId)?.email}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm">
-                      {(invoice.amount / 100).toLocaleString('en-US', {
-                        style: 'currency',
-                        currency: 'USD',
-                      })}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm">
-                      {formatDateToLocal(invoice.date)}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm">
-                      {renderInvoiceStatus(invoice.status)}
-                    </td>
-                    <td className="flex justify-end gap-2 whitespace-nowrap py-4 pl-3 pr-6 text-sm">
-                      <Link
-                        href={`/dashboard/invoices/${invoice.id}/edit`}
-                        className="rounded-md border p-1"
-                      >
-                        <PencilSquareIcon className="w-4" />
-                      </Link>
-                      <DeleteInvoice id={invoice.id} />
-                    </td>
+                    <th
+                      scope="col"
+                      className="relative py-3.5 pl-3 pr-4 sm:pr-6"
+                    >
+                      <span className="sr-only">Edit</span>
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody className="divide-y divide-gray-200 text-gray-500">
+                  {invoices.map((invoice) => (
+                    <tr key={invoice.id}>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-black sm:pl-6">
+                        {invoice.id}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm">
+                        <div className="flex items-center gap-3">
+                          <Image
+                            src={`${getCustomerById(invoice.customerId)
+                              ?.imageUrl}`}
+                            className="rounded-full"
+                            alt="Customer Image"
+                            width={28}
+                            height={28}
+                          />
+                          <p>{getCustomerById(invoice.customerId)?.name}</p>
+                        </div>
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm">
+                        {getCustomerById(invoice.customerId)?.email}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm">
+                        {(invoice.amount / 100).toLocaleString('en-US', {
+                          style: 'currency',
+                          currency: 'USD',
+                        })}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm">
+                        {formatDateToLocal(invoice.date)}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm">
+                        {renderInvoiceStatus(invoice.status)}
+                      </td>
+                      <td className="flex justify-end gap-2 whitespace-nowrap py-4 pl-3 pr-6 text-sm">
+                        <Link
+                          href={`/dashboard/invoices/${invoice.id}/edit`}
+                          className="rounded-md border p-1"
+                        >
+                          <PencilSquareIcon className="w-4" />
+                        </Link>
+                        <DeleteInvoice id={invoice.id} />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
