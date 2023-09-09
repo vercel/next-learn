35fc1ab86fa1fb6ead36d4a953e8ab233aae6b80
StephDietz <steph.dietz@vercel.com>
2023-09-09 10:09:26 -0500

    add type to invoice edit page

--- dashboard/15-final/app/dashboard/invoices/[id]/edit/page.tsx
+++ dashboard/15-final/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,11 +1,12 @@
 import InvoiceForm from '@/app/ui/invoices/form';
 import { notFound } from 'next/navigation';
 import { sql } from '@vercel/postgres';
+import { Invoice } from '@/app/lib/definitions';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id ? parseInt(params.id) : null;
   const invoiceData = await sql`SELECT * from INVOICES where id=${id}`;
-  const invoice = invoiceData.rows[0];
+  const invoice = invoiceData.rows[0] as Invoice;
 
   if (!invoice) {
     notFound();
