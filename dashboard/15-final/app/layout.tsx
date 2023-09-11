a2f795f0f400c44c8534dc1cc93f37d81f47696e
Delba de Oliveira <delbabrown@gmail.com>
2023-09-11 12:23:15 +0100

    Rename global -> globals

--- dashboard/15-final/app/layout.tsx
+++ dashboard/15-final/app/layout.tsx
@@ -1,7 +1,7 @@
-import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
+import './global.css'; // Delete
 
 export const metadata: Metadata = {
   title: 'Create Next App',
