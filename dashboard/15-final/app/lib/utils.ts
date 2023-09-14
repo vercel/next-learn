ea855823efcc77ef173a3eeaa33f0d4aebcb9c79
Delba de Oliveira <delbabrown@gmail.com>
2023-09-14 14:10:37 +0100

    Add temporary calculation

--- dashboard/15-final/app/lib/utils.ts
+++ dashboard/15-final/app/lib/utils.ts
@@ -7,6 +7,49 @@ export const formatCurrency = (amount: number) => {
   });
 };
 
+export function findLatestInvoices(invoices: Invoice[], customers: Customer[]) {
+  // Sort the invoices by date in descending order and take the top 5
+  const latestInvoices = [...invoices]
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    .slice(0, 5);
+
+  // Find corresponding customers for the latest 5 invoices
+  const latestInvoicesWithCustomerInfo = latestInvoices.map((invoice) => {
+    const customer = customers.find(
+      (customer) => customer.id === invoice.customer_id,
+    );
+
+    // Format the amount to USD
+    const formattedAmount = invoice.amount.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+
+    return {
+      id: invoice.id,
+      name: customer?.name,
+      image_url: customer?.image_url,
+      email: customer?.email,
+      amount: formattedAmount,
+    };
+  });
+
+  return latestInvoicesWithCustomerInfo;
+}
+
+export const calculateInvoicesByStatus = (
+  invoices: Invoice[],
+  status: 'pending' | 'paid',
+) => {
+  return invoices
+    .filter((invoice) => !status || invoice.status === status)
+    .reduce((total, invoice) => total + invoice.amount / 100, 0)
+    .toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+};
+
 export const calculateCustomerInvoices = (
   invoices: Invoice[],
   status: 'pending' | 'paid',
