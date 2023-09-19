8d5c7f5caddf8f17dd348aaa0866ddfaaf31444f
Delba de Oliveira <delbabrown@gmail.com>
2023-09-19 15:58:44 +0100

    Clean up

--- dashboard/15-final/app/ui/dashboard/card.tsx
+++ dashboard/15-final/app/ui/dashboard/card.tsx
@@ -18,7 +18,7 @@ export default function Card({
   type,
 }: {
   title: string;
-  value: number | string | undefined;
+  value: number | string;
   type: 'invoices' | 'customers' | 'pending' | 'collected';
 }) {
   const Icon = iconMap[type];
