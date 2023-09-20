fa4a3f63795b1fb38f0d7c9533edee9de45f86a1
Delba de Oliveira <delbabrown@gmail.com>
2023-09-20 16:43:27 +0100

    Update invoice definition

--- dashboard/15-final/app/lib/definitions.ts
+++ dashboard/15-final/app/lib/definitions.ts
@@ -18,11 +18,13 @@ export type Customer = {
 export type Invoice = {
   id: number;
   customer_id: number;
+  customer_name: string;
+  customer_email: string;
+  date: string;
   amount: number;
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
-  date: string;
 };
 
 export type LatestInvoice = {
