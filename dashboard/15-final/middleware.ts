963b6da27b50c3a49e5a3f60fee0f6668860c72c
StephDietz <steph.dietz@vercel.com>
2023-09-21 16:05:29 -0500

    middleware change

--- dashboard/15-final/middleware.ts
+++ dashboard/15-final/middleware.ts
@@ -1,18 +1,19 @@
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
-export { default } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+export async function middleware(request: NextRequest) {
+  const session = request.cookies.has('next-auth.session-token');
 
-export const config = { matcher: ['/(dashboard.*)'] };
-// export async function middleware(request: NextRequest) {
-//   const session = request.cookies.has('next-auth.session-token');
-//   console.log('session debugging', session);
-//   if (session && request.nextUrl.pathname.startsWith('/login')) {
-//     return NextResponse.redirect(new URL('/dashboard', request.url));
-//   }
+  if (
+    session &&
+    (request.nextUrl.pathname.startsWith('/login') ||
+      request.url.includes('/login'))
+  ) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
 
-//   if (!session && request.nextUrl.pathname.startsWith('/dashboard')) {
-//     return NextResponse.redirect(new URL('/login', request.url));
-//   }
-// }
+  if (!session && request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+}
 
-// export const config = { matcher: ['/(dashboard.*)'] };
+export const config = { matcher: ['/(dashboard.*)', '/login'] };
