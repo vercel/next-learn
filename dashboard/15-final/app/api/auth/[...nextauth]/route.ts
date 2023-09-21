4ec6ccc889e7155d9e447cfa9890665a7282c194
StephDietz <steph.dietz@vercel.com>
2023-09-21 14:51:18 -0500

    remove edge

--- dashboard/15-final/app/api/auth/[...nextauth]/route.ts
+++ dashboard/15-final/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/auth';
 
-export const runtime = 'edge'; // 'nodejs' is the default
-
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
