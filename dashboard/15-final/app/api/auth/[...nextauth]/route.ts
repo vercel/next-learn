d4b106e5c1570b545c0d6b6269cccfa29e2e8f5f
StephDietz <steph.dietz@vercel.com>
2023-09-21 15:03:18 -0500

    more debugging

--- dashboard/15-final/app/api/auth/[...nextauth]/route.ts
+++ dashboard/15-final/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,5 @@ import NextAuth from 'next-auth';
 import { authOptions } from '@/auth';
 
 const handler = NextAuth(authOptions);
-
+console.log('debugging handler', handler);
 export { handler as GET, handler as POST };
