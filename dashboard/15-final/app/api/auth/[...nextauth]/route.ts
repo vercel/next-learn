4de1d1e91ec0d898e27fd415aba283c237b8ed8b
StephDietz <steph.dietz@vercel.com>
2023-09-21 14:50:05 -0500

    add edge runtime

--- dashboard/15-final/app/api/auth/[...nextauth]/route.ts
+++ dashboard/15-final/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/auth';
 
+export const runtime = 'edge'; // 'nodejs' is the default
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
