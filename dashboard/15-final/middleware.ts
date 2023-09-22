bcf34d63788a4e11a149be953185eea4b06b1164
StephDietz <steph.dietz@vercel.com>
2023-09-22 11:35:51 -0500

    test adding withauth

--- dashboard/15-final/middleware.ts
+++ dashboard/15-final/middleware.ts
@@ -1,19 +1,30 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { withAuth } from 'next-auth/middleware';
 
-export async function middleware(request: NextRequest) {
-  const session = request.cookies.has('next-auth.session-token');
-  console.log('session', session);
-  if (
-    session &&
-    (request.nextUrl.pathname.startsWith('/login') ||
-      request.url.includes('/login'))
-  ) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-  }
+export default withAuth(
+  async function middleware(request: NextRequest) {
+    const session = request.cookies.has('next-auth.session-token');
+    console.log('session', session);
+    if (
+      session &&
+      (request.nextUrl.pathname.startsWith('/login') ||
+        request.url.includes('/login'))
+    ) {
+      return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
 
-  if (!session && request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-  return NextResponse.next();
-}
+    if (!session && request.nextUrl.pathname.startsWith('/dashboard')) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        console.log('authorized', token);
+        return token ? true : true;
+      },
+    },
+  },
+);
