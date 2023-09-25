c8094e74a9fe147b10c63d2a24e79c0c26d9841f
StephDietz <steph.dietz@vercel.com>
2023-09-25 11:53:36 -0500

    update middleware redirect

--- dashboard/15-final/middleware.ts
+++ dashboard/15-final/middleware.ts
@@ -1,30 +1,47 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { withAuth } from 'next-auth/middleware';
+import { getSession } from 'next-auth/react';
+
+// export default withAuth(
+//   async function middleware(req) {
+//     const session = req.nextauth.token;
+//     console.log('session', session);
+//     if (session && req.nextUrl.pathname.startsWith('/login')) {
+//       return NextResponse.redirect(new URL('/dashboard', req.url));
+//     } else if (!session && req.nextUrl.pathname.startsWith('/dashboard')) {
+//       return NextResponse.redirect(new URL('/login', req.url));
+//     } else {
+//       return NextResponse.next();
+//     }
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ token }) => {
+//         console.log('authorized', token);
+//         return token ? true : true;
+//       },
+//     },
+//   },
+// );
+// export { default } from 'next-auth/middleware';
 
 export default withAuth(
-  async function middleware(request: NextRequest) {
-    const session = request.cookies.has('next-auth.session-token');
-    console.log('session', session);
-    if (
-      session &&
-      (request.nextUrl.pathname.startsWith('/login') ||
-        request.url.includes('/login'))
-    ) {
-      return NextResponse.redirect(new URL('/dashboard', request.url));
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    const session = req.nextauth.token;
+    console.log(req);
+    if (session && req.nextUrl.pathname.startsWith('/login')) {
+      return NextResponse.redirect(new URL('/dashboard', req.url));
     }
-
-    if (!session && request.nextUrl.pathname.startsWith('/dashboard')) {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
-    return NextResponse.next();
   },
-  {
-    callbacks: {
-      authorized: ({ token }) => {
-        console.log('authorized', token);
-        return token ? true : true;
-      },
-    },
-  },
+  // {
+  //   callbacks: {
+  //     authorized: (req) => {
+  //       console.log('authorized', req);
+  //       return true;
+  //     },
+  //   },
+  // },
 );
+
+export const config = { matcher: ['/dashboard/:path*', '/login/:path*'] };
