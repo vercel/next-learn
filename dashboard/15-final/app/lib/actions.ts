8507afe18d92dda06ce4b8a276459cb7f9c121e1
Delba de Oliveira <delbabrown@gmail.com>
2023-09-25 11:29:32 +0100

    Update spelling mistake

--- dashboard/15-final/app/lib/actions.ts
+++ dashboard/15-final/app/lib/actions.ts
@@ -5,11 +5,6 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
-export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
-}
-
 const NewInvoice = z.object({
   customerId: z.string(),
   amount: z.coerce.number(),
@@ -24,11 +19,11 @@ export async function createInvoice(formData: FormData) {
   const rawFormData = Object.fromEntries(formData.entries());
   const { customerId, amount, status } = NewInvoice.parse(rawFormData);
   const date = new Date().toISOString().split('T')[0];
-  const ammountInCents = amount * 100;
+  const amountInCents = amount * 100;
 
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${ammountInCents}, ${status}, ${date})
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 
   revalidatePath('/dashboard/invoices');
@@ -39,14 +34,19 @@ export async function updateInvoice(formData: FormData) {
   const { id, customerId, amount, status } = UpdatedInvoice.parse(
     Object.fromEntries(formData.entries()),
   );
-  const ammountInCents = amount * 100;
+  const amountInCents = amount * 100;
 
   await sql`
     UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${ammountInCents}, status = ${status}
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
