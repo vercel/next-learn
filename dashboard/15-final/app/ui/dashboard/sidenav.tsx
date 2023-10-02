c93add3eef3e2639a506e0a4b8d7b60543089cb7
StephDietz <steph.dietz@vercel.com>
2023-10-02 15:49:32 -0500

    update side nav with new ui

--- dashboard/15-final/app/ui/dashboard/sidenav.tsx
+++ dashboard/15-final/app/ui/dashboard/sidenav.tsx
@@ -5,20 +5,23 @@ import LogOutButton from './log-out-button';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col justify-between border-r px-2 py-4">
-      <div>
-        <Link href="/">
-          <Image
-            src="/logo.png"
-            alt="The Next.js Symbol, a white N inside a black circle"
-            className="mb-4 ml-1"
-            width={32}
-            height={32}
-          />
-        </Link>
+    <div className="flex h-full flex-col border-r px-2 py-4">
+      <Link
+        className="flex h-20 items-end rounded-md bg-blue-600 p-4 md:h-40"
+        href="/"
+      >
+        <Image
+          src="/acme-logo.svg"
+          alt="The Next.js Symbol, a white N inside a black circle"
+          width={100}
+          height={100}
+        />
+      </Link>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col">
         <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <LogOutButton />
       </div>
-      <LogOutButton />
     </div>
   );
 }
