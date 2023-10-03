2b7e104de1e5cbc40bc7f474bc1e72c6eb200313
Stephanie Dietz <49788645+StephDietz@users.noreply.github.com>
2023-10-03 09:12:12 -0500

    Update dashboard/15-final/app/dashboard/(overview)/page.tsx
    Co-authored-by: Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>

--- dashboard/15-final/app/dashboard/(overview)/page.tsx
+++ dashboard/15-final/app/dashboard/(overview)/page.tsx
@@ -20,7 +20,7 @@ export default async function Page() {
 
   return (
     <main>
-      <h1 className={clsx(lusitana.className, 'mb-4 text-xl md:text-2xl')}>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
