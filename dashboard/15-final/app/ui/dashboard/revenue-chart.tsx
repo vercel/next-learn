9e217511b4eb9a10a5668996d6d10a96ccbdf632
Stephanie Dietz <49788645+StephDietz@users.noreply.github.com>
2023-10-03 09:31:49 -0500

    dash home page UI (#185)
    * dash home page UI
    
    * add lusitana font
    
    * move fonts file to ui
    
    * Update dashboard/15-final/app/ui/dashboard/latest-invoices.tsx
    
    Co-authored-by: Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
    
    * Update dashboard/15-final/app/dashboard/(overview)/page.tsx
    
    Co-authored-by: Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
    
    * merge main
    
    ---------
    
    Co-authored-by: Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>

--- dashboard/15-final/app/ui/dashboard/revenue-chart.tsx
+++ dashboard/15-final/app/ui/dashboard/revenue-chart.tsx
@@ -1,6 +1,8 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { fetchRevenue } from '@/app/lib/data';
-
+import { CalendarIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+import clsx from 'clsx';
 // This component is representational only.
 // For data visualization UI, check out:
 // https://www.tremor.so/
@@ -18,34 +20,42 @@ export default async function RevenueChart() {
   }
 
   return (
-    <div className="rounded-xl border bg-white p-6 shadow-sm md:col-span-5">
-      <h2 className="font-semibold">Revenue</h2>
-      <div className="sm:grid-cols-13 mt-4 grid grid-cols-12 items-end gap-2 md:gap-4">
-        {/* y-axis */}
-        <div
-          className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
-          style={{ height: `${chartHeight}px` }}
-        >
-          {yAxisLabels.map((label) => (
-            <p key={label}>{label}</p>
+    <div className="w-full md:col-span-4">
+      <h2 className={clsx(lusitana.className, 'mb-4 text-xl md:text-2xl')}>
+        Recent Revenue
+      </h2>
+      <div className="rounded-xl bg-gray-50 p-4">
+        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
+          {/* y-axis */}
+          <div
+            className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
+            style={{ height: `${chartHeight}px` }}
+          >
+            {yAxisLabels.map((label) => (
+              <p key={label}>{label}</p>
+            ))}
+          </div>
+
+          {revenue.map((month) => (
+            <div key={month.month} className="flex flex-col items-center gap-2">
+              {/* bars */}
+              <div
+                className="w-full rounded-md bg-blue-300"
+                style={{
+                  height: `${(chartHeight / topLabel) * month.revenue}px`,
+                }}
+              ></div>
+              {/* x-axis */}
+              <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
+                {month.month}
+              </p>
+            </div>
           ))}
         </div>
-
-        {revenue.map((month) => (
-          <div key={month.month} className="flex flex-col items-center gap-2">
-            {/* bars */}
-            <div
-              className="w-full rounded-md bg-blue-300"
-              style={{
-                height: `${(chartHeight / topLabel) * month.revenue}px`,
-              }}
-            ></div>
-            {/* x-axis */}
-            <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
-              {month.month}
-            </p>
-          </div>
-        ))}
+        <div className="flex items-center pb-2 pt-6">
+          <CalendarIcon className="h-5 w-5 text-gray-500" />
+          <h3 className="ml-2 text-sm text-gray-500 ">Last 6 months</h3>
+        </div>
       </div>
     </div>
   );
