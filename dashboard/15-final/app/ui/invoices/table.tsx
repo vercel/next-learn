f35a180f860231594416add89245c5601e6b0b06
StephDietz <steph.dietz@vercel.com>
2023-10-03 14:06:13 -0500

    add mobile table styling

--- dashboard/15-final/app/ui/invoices/table.tsx
+++ dashboard/15-final/app/ui/invoices/table.tsx
@@ -13,9 +13,46 @@ export default async function InvoicesTable({
     <div className="mt-4 flow-root">
       <div className="overflow-x-auto">
         <div className="inline-block min-w-full align-middle">
-          <div className="overflow-hidden rounded-md bg-gray-50 p-4">
-            <table className="min-w-full rounded-md bg-white">
-              <thead className="hidden rounded-md bg-gray-50 text-left text-sm">
+          <div className="overflow-hidden rounded-md bg-gray-50 p-2">
+            <div className="md:hidden">
+              {invoices?.map((invoice) => (
+                <div
+                  key={invoice.id}
+                  className="mb-2 w-full rounded-md bg-white p-4"
+                >
+                  <div className="flex items-center justify-between border-b pb-4">
+                    <div>
+                      <div className="mb-2 flex items-center">
+                        <Image
+                          src={invoice.image_url}
+                          className="mr-2 rounded-full"
+                          width={28}
+                          height={28}
+                          alt={`${invoice.name}'s profile picture`}
+                        />
+                        <p>{invoice.name}</p>
+                      </div>
+                      <p className="text-sm text-gray-500">{invoice.email}</p>
+                    </div>
+                    <InvoiceStatus status={invoice.status} />
+                  </div>
+                  <div className="flex w-full items-center justify-between pt-4">
+                    <div>
+                      <p className="text-xl font-medium">
+                        {formatCurrency(invoice.amount)}
+                      </p>
+                      <p>{formatDateToLocal(invoice.date)}</p>
+                    </div>
+                    <div className="flex justify-end gap-2">
+                      <UpdateInvoice id={invoice.id} />
+                      <DeleteInvoice id={invoice.id} />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <table className="hidden min-w-full rounded-md bg-white md:table">
+              <thead className="rounded-md bg-gray-50 text-left text-sm">
                 <tr>
                   <th
                     scope="col"
@@ -54,7 +91,7 @@ export default async function InvoicesTable({
               </thead>
               <tbody className="divide-y divide-gray-200 text-gray-900">
                 {invoices?.map((invoice) => (
-                  <tr key={invoice.id}>
+                  <tr key={invoice.id} className="w-full">
                     <td className="whitespace-nowrap px-6 py-5">
                       <div className="flex items-center gap-3">
                         <Image
