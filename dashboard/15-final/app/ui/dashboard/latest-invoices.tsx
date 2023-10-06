4be9de1269fad4aa5aa4da19e19af7f60aae623e
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2023-10-06 15:58:05 +0100

    Stream Latest Invoices (#202)
    Co-authored-by: Stephanie Dietz <49788645+StephDietz@users.noreply.github.com>

--- dashboard/15-final/app/ui/dashboard/latest-invoices.tsx
+++ dashboard/15-final/app/ui/dashboard/latest-invoices.tsx
@@ -1,14 +1,12 @@
-import { LatestInvoice } from '@/app/lib/definitions';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchLatestInvoices } from '@/app/lib/data';
 
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices();
+
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -45,7 +43,7 @@ export default async function LatestInvoices({
                   </div>
                 </div>
                 <p
-                  className={`${lusitana.className} md:text-base, truncate text-sm font-medium`}
+                  className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
                 >
                   {invoice.amount}
                 </p>
--- dashboard/15-final/app/ui/dashboard/revenue-chart.tsx
+++ dashboard/15-final/app/ui/dashboard/revenue-chart.tsx
@@ -1,7 +1,8 @@
 import { generateYAxis } from '@/app/lib/utils';
-import { fetchRevenue } from '@/app/lib/data';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
+
 // This component is representational only.
 // For data visualization UI, check out:
 // https://www.tremor.so/
