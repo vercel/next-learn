a350aed7047015489505e45f0783335c0bb8d4f8
Delba de Oliveira <delbabrown@gmail.com>
2023-10-09 15:26:49 +0100

    Fix border bug for single page

--- dashboard/15-final/app/ui/invoices/pagination.tsx
+++ dashboard/15-final/app/ui/invoices/pagination.tsx
@@ -34,10 +34,11 @@ export default function Pagination({
 
       <div className="flex -space-x-px">
         {allPages.map((page, index) => {
-          let position: 'first' | 'last' | 'middle' | undefined;
+          let position: 'first' | 'last' | 'single' | 'middle' | undefined;
 
           if (index === 0) position = 'first';
           if (index === allPages.length - 1) position = 'last';
+          if (allPages.length === 1) position = 'single';
           if (page === '...') position = 'middle';
 
           return (
@@ -69,14 +70,14 @@ function PaginationNumber({
 }: {
   page: number | string;
   href: string;
-  position?: 'first' | 'last' | 'middle';
+  position?: 'first' | 'last' | 'middle' | 'single';
   isActive: boolean;
 }) {
   const className = clsx(
     'flex h-10 w-10 items-center justify-center text-sm border',
     {
-      'rounded-l-md': position === 'first',
-      'rounded-r-md': position === 'last',
+      'rounded-l-md': position === 'first' || position === 'single',
+      'rounded-r-md': position === 'last' || position === 'single',
       'z-10 bg-blue-600 border-blue-600 text-white': isActive,
       'hover:bg-gray-100': !isActive && position !== 'middle',
       'text-gray-300': position === 'middle',
