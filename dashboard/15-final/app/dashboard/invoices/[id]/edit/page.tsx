a4d66e65272fbf3e7b17e714d9fc6f2994634a80
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2023-10-19 15:37:54 +0100

    Add basic metadata and update OG image (#212)
    * Bump nextauth
    
    * Move data fetching function to data.ts
    
    - We don't ask the user to add this in the chapter
    
    * Fix imports
    
    * Misc
    
    * Addd metadata
    
    * Add new OG image

--- dashboard/15-final/app/dashboard/invoices/[id]/edit/page.tsx
+++ dashboard/15-final/app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,11 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
--- dashboard/15-final/app/lib/data.ts
+++ dashboard/15-final/app/lib/data.ts
@@ -5,6 +5,7 @@ import {
   InvoiceForm,
   InvoicesTable,
   LatestInvoiceRaw,
+  User,
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
@@ -239,5 +240,15 @@ export async function fetchFilteredCustomers(query: string) {
   } catch (err) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch customer table.');
+  }
+}
+
+export async function getUser(email: string) {
+  try {
+    const user = await sql`SELECT * from USERS where email=${email}`;
+    return user.rows[0] as User;
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
   }
 }
