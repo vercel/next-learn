b1b4919bc788a03ac2669c14c5c17abe65b71183
Delba de Oliveira <delbabrown@gmail.com>
2023-10-21 07:13:01 -0700

    Create wrapper component for Cards

--- dashboard/15-final/app/ui/dashboard/cards.tsx
+++ dashboard/15-final/app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,7 +14,29 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default function Card({
+export default async function Cards() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
+  return (
+    <>
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Total Customers"
+        value={numberOfCustomers}
+        type="customers"
+      />
+    </>
+  );
+}
+
+export function Card({
   title,
   value,
   type,
