23525f2319d130480f116effe7167c39c632da2d
Delba de Oliveira <delbabrown@gmail.com>
2023-10-21 17:17:43 -0700

    Update page.tsx

--- dashboard/15-final/app/dashboard/(overview)/page.tsx
+++ dashboard/15-final/app/dashboard/(overview)/page.tsx
@@ -1,8 +1,7 @@
-import Card from '@/app/ui/dashboard/cards';
+import Cards from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
@@ -19,7 +18,7 @@ export default async function Page() {
       <h1>{Date.now()}</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
-          <Card />
+          <Cards />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
