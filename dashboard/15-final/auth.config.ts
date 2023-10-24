b3c9a9cf1778334ae8cd80d2481e21125556b937
Balázs Orbán <info@balazsorban.com>
2023-10-24 15:52:49 -0700

    tweaks

--- dashboard/15-final/auth.config.ts
+++ dashboard/15-final/auth.config.ts
@@ -1,23 +1,20 @@
 import type { NextAuthConfig } from 'next-auth';
-import { NextResponse } from 'next/server';
 
 export const authConfig = {
   providers: [
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
+    // while this file is also used in non-Node.js environments
   ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
-        nextUrl.pathname = '/login';
-        if (!isLoggedIn) return NextResponse.redirect(nextUrl);
+        if (!isLoggedIn) return Response.redirect(new URL('/login', nextUrl));
         return true;
       } else if (isLoggedIn) {
-        nextUrl.pathname = '/dashboard';
-        return NextResponse.redirect(nextUrl);
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
-
       return true;
     },
   },
