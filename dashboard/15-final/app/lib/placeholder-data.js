8c4ea4cfab2ceae32ba877479606fe51ac057df0
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2023-10-25 15:12:02 +0100

    Update dashboard/15-final/app/lib/placeholder-data.js

--- dashboard/15-final/app/lib/placeholder-data.js
+++ dashboard/15-final/app/lib/placeholder-data.js
@@ -1,4 +1,4 @@
-// This file contains placeholder data that you'll be replacing with real data in Fetching Data chapter:
+// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
 // https://nextjs.org/learn/dashboard-app/fetching-data
 const users = [
   {
