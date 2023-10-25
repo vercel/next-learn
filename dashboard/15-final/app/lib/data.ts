31df9812b60daff839a02348321af8a59602a433
Delba de Oliveira <delbabrown@gmail.com>
2023-10-24 17:23:55 -0700

    Delete unused data fetch

--- dashboard/15-final/app/lib/data.ts
+++ dashboard/15-final/app/lib/data.ts
@@ -190,36 +190,6 @@ export async function fetchCustomers() {
   }
 }
 
-export async function fetchCustomersTable() {
-  try {
-    const data = await sql<CustomersTable>`
-      SELECT
-        customers.id,
-        customers.name,
-        customers.email,
-        customers.image_url,
-        COUNT(invoices.id) AS total_invoices,
-        SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
-        SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
-      FROM customers
-      LEFT JOIN invoices ON customers.id = invoices.customer_id
-      GROUP BY customers.id, customers.name, customers.email, customers.image_url
-      ORDER BY customers.name ASC
-    `;
-
-    const customers = data.rows.map((customer) => ({
-      ...customer,
-      total_pending: formatCurrency(customer.total_pending),
-      total_paid: formatCurrency(customer.total_paid),
-    }));
-
-    return customers;
-  } catch (err) {
-    console.error('Database Error:', err);
-    throw new Error('Failed to fetch customer table.');
-  }
-}
-
 export async function fetchFilteredCustomers(query: string) {
   try {
     const data = await sql<CustomersTable>`
