962fe4507385f321f6cc543768f5783faba64a04
Balázs Orbán <info@balazsorban.com>
2023-10-25 20:42:34 +0200

    Update page.tsx

--- dashboard/15-final/app/login/page.tsx
+++ dashboard/15-final/app/login/page.tsx
@@ -5,7 +5,7 @@ import { AtSymbolIcon, KeyIcon } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '../ui/button';
 
-export default async function LoginForm() {
+export default async function Page() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
