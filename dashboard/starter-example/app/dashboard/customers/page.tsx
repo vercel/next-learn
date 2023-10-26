f4eb6239c53c15617e3d8e704f96af1df554eaaa
Delba de Oliveira <delbabrown@gmail.com>
2023-10-26 08:57:28 -0700

    Add code for chapter 15

--- dashboard/starter-example/app/dashboard/customers/page.tsx
+++ dashboard/starter-example/app/dashboard/customers/page.tsx
@@ -1,6 +1,5 @@
 import { fetchFilteredCustomers } from '@/app/lib/data';
 import CustomersTable from '@/app/ui/customers/table';
-import { Metadata } from 'next';
 
 export default async function Page({
   searchParams,
