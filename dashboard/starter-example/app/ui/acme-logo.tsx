5ebfe4885eee0d4165fbe19f6823ba8269b949ee
Delba de Oliveira <delbabrown@gmail.com>
2023-10-26 09:56:09 -0700

    :nail_care:

--- dashboard/starter-example/app/ui/acme-logo.tsx
+++ dashboard/starter-example/app/ui/acme-logo.tsx
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px] ">Acme</p>
+      <p className="text-[44px]">Acme</p>
     </div>
   );
 }
