ad6ef574e786c6a7776e04ce7f999cb7045a1804
StephDietz <steph.dietz@vercel.com>
2023-10-26 09:30:41 -0700

    chapter 7

--- dashboard/starter-example/app/ui/dashboard/latest-invoices.tsx
+++ dashboard/starter-example/app/ui/dashboard/latest-invoices.tsx
@@ -2,10 +2,7 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices } from '@/app/lib/data';
 export default async function LatestInvoices() {
-  const latestInvoices = await fetchLatestInvoices();
-
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
