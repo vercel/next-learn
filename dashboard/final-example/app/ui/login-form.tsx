1f4c96b55ad01d26f7329b170a1836c9e6475e82
Steven Tey <stevensteel97@gmail.com>
2023-10-25 18:23:57 -0700

    fixed images and added login button pending state

--- dashboard/final-example/app/ui/login-form.tsx
+++ dashboard/final-example/app/ui/login-form.tsx
@@ -13,7 +13,6 @@ import { useFormState, useFormStatus } from 'react-dom';
 
 export default function LoginForm() {
   const [code, action] = useFormState(authenticate, undefined);
-  const { pending } = useFormStatus();
 
   return (
     <form action={action} className="space-y-3">
@@ -62,9 +61,7 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full" aria-disabled={pending}>
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
+        <LoginButton />
         <div className="flex h-8 items-end space-x-1">
           {code === 'CredentialsSignin' && (
             <>
@@ -77,5 +74,15 @@ export default function LoginForm() {
         </div>
       </div>
     </form>
+  );
+}
+
+function LoginButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button className="mt-4 w-full" aria-disabled={pending}>
+      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    </Button>
   );
 }
