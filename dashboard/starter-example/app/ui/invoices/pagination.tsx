94849b458bcec11ebcff349d057b06a39987e456
Delba de Oliveira <delbabrown@gmail.com>
2023-10-26 09:32:27 -0700

    Chapter 11

--- dashboard/starter-example/app/ui/invoices/pagination.tsx
+++ dashboard/starter-example/app/ui/invoices/pagination.tsx
@@ -4,19 +4,8 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
-import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
-
-  const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams);
-    params.set('page', pageNumber.toString());
-    return `${pathname}?${params.toString()}`;
-  };
-
   const allPages = generatePagination(currentPage, totalPages);
 
   return (
