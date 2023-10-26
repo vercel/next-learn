ddb2327c8c4ff39bfbc2c697cae805eeadc55a4a
StephDietz <steph.dietz@vercel.com>
2023-10-26 11:51:50 -0700

    comment out code that throws error when trying to deploy to vercel

--- dashboard/starter-example/app/ui/dashboard/revenue-chart.tsx
+++ dashboard/starter-example/app/ui/dashboard/revenue-chart.tsx
@@ -10,20 +10,19 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default async function RevenueChart() {
   const chartHeight = 350;
-  const { yAxisLabels, topLabel } = generateYAxis(revenue);
+  // const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
-  if (!revenue || revenue.length === 0) {
-    return <p className="mt-4 text-gray-400">No data available.</p>;
-  }
+  // if (!revenue || revenue.length === 0) {
+  //   return <p className="mt-4 text-gray-400">No data available.</p>;
+  // }
 
   return (
     <div className="w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Recent Revenue
       </h2>
-      <div className="rounded-xl bg-gray-50 p-4">
+      {/* <div className="rounded-xl bg-gray-50 p-4">
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
-          {/* y-axis */}
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}
@@ -35,14 +34,12 @@ export default async function RevenueChart() {
 
           {revenue.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
-              {/* bars */}
               <div
                 className="w-full rounded-md bg-blue-300"
                 style={{
                   height: `${(chartHeight / topLabel) * month.revenue}px`,
                 }}
               ></div>
-              {/* x-axis */}
               <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
                 {month.month}
               </p>
@@ -53,7 +50,7 @@ export default async function RevenueChart() {
           <CalendarIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- dashboard/starter-example/app/ui/search.tsx
+++ dashboard/starter-example/app/ui/search.tsx
@@ -2,27 +2,27 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
+// import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
 
-  const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
+  // const handleSearch = useDebouncedCallback((term) => {
+  //   console.log(`Searching... ${term}`);
 
-    const params = new URLSearchParams(searchParams);
+  //   const params = new URLSearchParams(searchParams);
 
-    params.set('page', '1');
+  //   params.set('page', '1');
 
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-  }, 300);
+  //   if (term) {
+  //     params.set('query', term);
+  //   } else {
+  //     params.delete('query');
+  //   }
+  //   replace(`${pathname}?${params.toString()}`);
+  // }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -33,7 +33,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          handleSearch(e.target.value);
+          // handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
       />
