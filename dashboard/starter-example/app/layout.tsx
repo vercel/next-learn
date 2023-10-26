c8a192c0ca6a267e9a0ae4f02009b48519248658
StephDietz <steph.dietz@vercel.com>
2023-10-26 09:06:55 -0700

    first 3 chapters

--- dashboard/starter-example/app/layout.tsx
+++ dashboard/starter-example/app/layout.tsx
@@ -1,15 +1,3 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
-  },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
-};
 export default function RootLayout({
   children,
 }: {
@@ -17,7 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
