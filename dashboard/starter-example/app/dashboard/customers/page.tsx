401763aa2d69c90d8c9914b8dc65f36cf67d1021
Delba de Oliveira <delbabrown@gmail.com>
2023-10-26 08:47:33 -0700

    Remove code for chapter 16

--- dashboard/starter-example/app/dashboard/customers/page.tsx
+++ dashboard/starter-example/app/dashboard/customers/page.tsx
@@ -2,10 +2,6 @@ import { fetchFilteredCustomers } from '@/app/lib/data';
 import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Customers',
-};
-
 export default async function Page({
   searchParams,
 }: {
--- dashboard/starter-example/app/layout.tsx
+++ dashboard/starter-example/app/layout.tsx
@@ -1,15 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
-  },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
-};
 export default function RootLayout({
   children,
 }: {
