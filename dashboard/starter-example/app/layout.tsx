754ebf1a8afd1f8831f6f5dacf1ce512bc12add4
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2023-10-31 05:10:50 +0530

    Fix #319 Cannot find module '@/app/ui/fonts' error

--- dashboard/starter-example/app/layout.tsx
+++ dashboard/starter-example/app/layout.tsx
@@ -1,3 +1,5 @@
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
