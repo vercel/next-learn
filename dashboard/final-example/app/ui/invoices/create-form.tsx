c14719dc901c415e0619ccb76a06a780b7a68886
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-10-31 22:22:41 +0530

    Removed Cursor Pointer from text Input

--- dashboard/final-example/app/ui/invoices/create-form.tsx
+++ dashboard/final-example/app/ui/invoices/create-form.tsx
@@ -70,7 +70,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 type="number"
                 step="0.01"
                 placeholder="Enter USD amount"
-                className="peer block cursor-pointer w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
