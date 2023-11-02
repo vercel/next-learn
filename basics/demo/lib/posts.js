2a77c1c7655d5e8d982a87cde406c9888087508a
umuturgunsoy <70551985+umuturgunsoy@users.noreply.github.com>
2023-11-02 17:18:21 +0300

    Update posts.js
    The wrong object name in the code example in the tutorial at https://nextjs.org/learn-pages-router/basics/data-fetching/blog-data has been fixed.

--- basics/demo/lib/posts.js
+++ basics/demo/lib/posts.js
@@ -9,12 +9,12 @@ const postsDirectory = path.join(process.cwd(), 'posts');
 export function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
+  const allPostsData = fileNames.map((fileNames) => {
     // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '');
+    const id = fileNames.replace(/\.md$/, '');
 
     // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
+    const fullPath = path.join(postsDirectory, fileNames);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
 
     // Use gray-matter to parse the post metadata section
