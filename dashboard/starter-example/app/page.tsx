145efbb7b8de7786d720ba7eab00da05879166e9
StephDietz <steph.dietz@vercel.com>
2023-11-08 13:13:18 -0600

    remove Link component from page. This will be added later

--- dashboard/starter-example/app/page.tsx
+++ dashboard/starter-example/app/page.tsx
@@ -1,7 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
-import Link from 'next/link';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -17,12 +16,12 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <Link
+          <a
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+          </a>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
