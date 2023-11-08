aa1d2ac93c7afc379eb59b29237a691c4fd77b48
StephDietz <steph.dietz@vercel.com>
2023-11-08 13:53:59 -0600

    re-add the Link component. Turns out is was supposed to be there

--- dashboard/starter-example/app/page.tsx
+++ dashboard/starter-example/app/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 export default function Page() {
   return (
@@ -16,12 +17,12 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <a
+          <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </a>
+          </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
