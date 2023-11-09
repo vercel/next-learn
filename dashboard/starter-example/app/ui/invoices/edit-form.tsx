cd928d926fc46eb4fdda9b069e076197e075a252
Luther Tchofo Safo <108332538+LutherTS@users.noreply.github.com>
2023-11-09 15:35:56 +0100

    Update edit-form.tsx
    step="0.01" added in "Invoice Amount from create-form. Was blocking HTML form validation in a weird way.

--- dashboard/starter-example/app/ui/invoices/edit-form.tsx
+++ dashboard/starter-example/app/ui/invoices/edit-form.tsx
@@ -56,6 +56,7 @@ export default function EditInvoiceForm({
                 id="amount"
                 name="amount"
                 type="number"
+                step="0.01"
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
