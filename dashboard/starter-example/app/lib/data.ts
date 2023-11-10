22c2446e88097136436eaf15f419e4b4f19ce1ae
Kenny Elshoff <github@kennyelshoff.com>
2023-11-09 22:25:41 -0500

    fix: throw new error in `fetchInvoiceById` catch block

--- dashboard/starter-example/app/lib/data.ts
+++ dashboard/starter-example/app/lib/data.ts
@@ -165,6 +165,7 @@ export async function fetchInvoiceById(id: string) {
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
+    throw new Error('Failed to fetch invoice.');
   }
 }
 
