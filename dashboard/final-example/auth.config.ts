8c522feed010b17ba560434b1e01fe2143296e14
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2023-11-09 16:41:06 -0800

    update auth config to allow images (#400)

--- dashboard/final-example/auth.config.ts
+++ dashboard/final-example/auth.config.ts
@@ -11,8 +11,12 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
+      const protectedPaths = ['/dashboard', '/customers', '/invoices'];
+      const isProtectedPath = protectedPaths.some((path) =>
+        nextUrl.pathname.startsWith(path),
+      );
+
+      if (isProtectedPath) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
