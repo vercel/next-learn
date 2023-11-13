b1052e5b676106832323f64cc1ed853ff86cba8a
StephDietz <steph.dietz@vercel.com>
2023-11-13 09:51:45 -0600

    replace flex-grow with the new utility grow

--- dashboard/final-example/app/dashboard/layout.tsx
+++ dashboard/final-example/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
 }
