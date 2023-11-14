80f7f661954714c06cf930e38ac1236507c0f745
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-11-14 05:40:41 -0300

    Fix: If currentPage is NaN throws error
    Added verification for currentPage so it doesn't breaks the code when there's no page set

--- dashboard/final-example/app/dashboard/invoices/page.tsx
+++ dashboard/final-example/app/dashboard/invoices/page.tsx
@@ -21,7 +21,11 @@ export default async function Page({
   };
 }) {
   const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
+  let currentPage = Number(searchParams?.page) || 1;
+
+  if(Number.isNaN(currentPage)){
+    currentPage = 1;
+  }
 
   const totalPages = await fetchInvoicesPages(query);
 
