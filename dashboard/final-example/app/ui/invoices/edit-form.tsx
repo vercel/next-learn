32607c93c9cd986c1c3ba9eb0c2b4955c233a6e7
Stephanie Dietz <49788645+StephDietz@users.noreply.github.com>
2023-11-16 09:22:40 -0600

    Edits based on feedback (#410)
    * remove Link component from page. This will be added later
    
    * re-add the Link component. Turns out is was supposed to be there
    
    * replace flex-grow with the new utility grow
    
    * issue: https://github.com/vercel/next-learn/pull/415
    
    * Add missing step
    
    ---------
    
    Co-authored-by: Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
    Co-authored-by: Delba de Oliveira <delbabrown@gmail.com>

--- dashboard/final-example/app/ui/invoices/edit-form.tsx
+++ dashboard/final-example/app/ui/invoices/edit-form.tsx
@@ -73,6 +73,7 @@ export default function EditInvoiceForm({
                 name="amount"
                 type="number"
                 defaultValue={invoice.amount}
+                step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
