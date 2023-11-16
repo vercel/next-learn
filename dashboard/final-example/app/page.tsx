fc281ac41c6eb6e821e264869c13bbb2dad94758
Delba de Oliveira <delbabrown@gmail.com>
2023-11-16 14:46:51 +0000

    Fix import order

--- dashboard/final-example/app/page.tsx
+++ dashboard/final-example/app/page.tsx
@@ -1,8 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Page() {
   return (
