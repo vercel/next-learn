45fcde471b8150ae22b48d3a803342c6aa4f0108
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2023-11-16 14:27:20 +0000

    Improve Form Error Accessibility  (#450)

--- dashboard/final-example/app/ui/login-form.tsx
+++ dashboard/final-example/app/ui/login-form.tsx
@@ -62,13 +62,15 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
-        <div className="flex h-8 items-end space-x-1">
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
           {state === 'CredentialsSignin' && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p aria-live="polite" className="text-sm text-red-500">
-                Invalid credentials
-              </p>
+              <p className="text-sm text-red-500">Invalid credentials</p>
             </>
           )}
         </div>
