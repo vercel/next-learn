77be0424f9cb688156e2dac38dae394f268165e9
Delba de Oliveira <delbabrown@gmail.com>
2023-11-17 09:05:27 +0000

    Add timeout

--- dashboard/final-example/app/lib/data.ts
+++ dashboard/final-example/app/lib/data.ts
@@ -20,7 +20,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
