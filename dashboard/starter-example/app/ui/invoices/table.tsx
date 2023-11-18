b126a7508cae8e034883147195ba108ad4cd7d68
taha-cmyk <151395216+taha-cmyk@users.noreply.github.com>
2023-11-18 19:04:29 +0100

    Update table.tsx
    fix alt is missing in Image when try to build the app in vercel

--- dashboard/starter-example/app/ui/invoices/table.tsx
+++ dashboard/starter-example/app/ui/invoices/table.tsx
@@ -27,6 +27,7 @@ export default async function InvoicesTable({
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
+                        alt=""
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
@@ -85,6 +86,7 @@ export default async function InvoicesTable({
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
                       <Image
+                        alt=""
                         src={invoice.image_url}
                         className="rounded-full"
                         width={28}
