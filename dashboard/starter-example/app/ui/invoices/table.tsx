7ad0bcd7e6a64d405f769a7ca5d81867dda20b02
ernestd <lapapelera@gmail.com>
2023-11-24 21:44:10 -0500

    Fixes #480. Add missing alt attribute that breaks build

--- dashboard/starter-example/app/ui/invoices/table.tsx
+++ dashboard/starter-example/app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt={`${invoice.name}'s profile picture`}
                       />
                       <p>{invoice.name}</p>
                     </div>
