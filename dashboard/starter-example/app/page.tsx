ad26f46321ca74d39e41bcf9f38e14df80117afb
Wenting Yong <wyong@orangerine.com>
2024-03-21 10:59:48 -0400

    Optimized the import code and remove the antialiased from page.

--- dashboard/starter-example/app/page.tsx
+++ dashboard/starter-example/app/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
           {/* <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" /> */}
           <div className={styles.shape} />
           <p
-            className={`${lusitana.className} text-xl text-gray-800 antialiased md:text-3xl md:leading-normal`}
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
--- dashboard/starter-example/app/ui/fonts.ts
+++ dashboard/starter-example/app/ui/fonts.ts
@@ -1,5 +1,4 @@
-import { Inter } from 'next/font/google';
-import { Lusitana } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
  
 export const inter = Inter({ subsets: ['latin'] });
