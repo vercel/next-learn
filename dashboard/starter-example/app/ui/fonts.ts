bccc4752407c72cf94f1d85aec5e8765510879a0
Wenting Yong <wyong@orangerine.com>
2024-03-21 10:39:04 -0400

    Added secondary font in page.tsx

--- dashboard/starter-example/app/ui/fonts.ts
+++ dashboard/starter-example/app/ui/fonts.ts
@@ -1,3 +1,6 @@
 import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
  
 export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ["400", "700"] });
