7cc936ba5beeeb41978dd33a27a13590f77f1c41
Howie Sommerfeld <howiesommerfeld@gmail.com>
2024-04-09 15:01:30 +0200

    Add index to key for uniqueness
    Using only the page as a key does not preserve uniqueness across updates. This caused, for example, an ellipsis to appear before the pagination elements when navigating from a page in the middle back to the first page.

--- dashboard/final-example/app/ui/invoices/pagination.tsx
+++ dashboard/final-example/app/ui/invoices/pagination.tsx
@@ -38,7 +38,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
           return (
             <PaginationNumber
-              key={page}
+              key={`${page}-${index}`}
               href={createPageURL(page)}
               page={page}
               position={position}
