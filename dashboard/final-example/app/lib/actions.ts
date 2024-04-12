a2dfb0d6487c300e8732e303b4521e41ef9f32aa
Roman Roibu <roman@roibu.xyz>
2024-04-12 23:21:11 +0200

    Replace invalid_type_error with required_error zod error message

--- dashboard/final-example/app/lib/actions.ts
+++ dashboard/final-example/app/lib/actions.ts
@@ -10,13 +10,13 @@ import { AuthError } from 'next-auth';
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
-    invalid_type_error: 'Please select a customer.',
+    required_error: 'Please select a customer.',
   }),
   amount: z.coerce
     .number()
     .gt(0, { message: 'Please enter an amount greater than $0.' }),
   status: z.enum(['pending', 'paid'], {
-    invalid_type_error: 'Please select an invoice status.',
+    required_error: 'Please select an invoice status.',
   }),
   date: z.string(),
 });
