57c954653222dc024e4284ccccb41d8b2a30d296
Delba de Oliveira <delbabrown@gmail.com>
2024-06-05 09:29:15 +0100

    Remove next-auth

--- dashboard/final-example/app/lib/actions.ts
+++ dashboard/final-example/app/lib/actions.ts
@@ -4,8 +4,6 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -124,17 +122,5 @@ export async function authenticate(
   prevState: string | undefined,
   formData: FormData,
 ) {
-  try {
-    await signIn('credentials', formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
-    }
-    throw error;
-  }
+  return null
 }
