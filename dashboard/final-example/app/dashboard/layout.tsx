deca7c766f4b33f9af1a9b2f0475f253f9abefc3
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2024-06-19 08:50:40 +0100

    Add PPR (#745)
    * Update to next v15, switch to pnpm
    
    * Delete package-lock.json
    
    * Add PPR
    
    * Update to next@canary
    
    * Update starter-example to next@canary
    
    * Update file extension
    
    * Update ts compiler options to match CNA
    
    * Fix ts errors

--- dashboard/final-example/app/dashboard/layout.tsx
+++ dashboard/final-example/app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
+export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
--- dashboard/final-example/app/ui/invoices/create-form.tsx
+++ dashboard/final-example/app/ui/invoices/create-form.tsx
@@ -9,11 +9,11 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/actions';
+import { createInvoice, State } from '@/app/lib/actions';
 import { useFormState } from 'react-dom';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  const initialState = { message: null, errors: {} };
+  const initialState: State = { message: null, errors: {} };
   const [state, dispatch] = useFormState(createInvoice, initialState);
 
   return (
