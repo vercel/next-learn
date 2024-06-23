dbccff17276aaddac18cd39d24d5269bd84897cd
Lee Robinson <lrobinson2011@gmail.com>
2024-06-23 17:44:44 -0500

    Push it

--- dashboard/final-example/app/ui/invoices/buttons.tsx
+++ dashboard/final-example/app/ui/invoices/buttons.tsx
@@ -30,7 +30,7 @@ export function DeleteInvoice({ id }: { id: string }) {
 
   return (
     <form action={deleteInvoiceWithId}>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
+      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
