bcffa78bab929470439d138d058fceef4cefa7c2
mdmahendri <mahendri.md@gmail.com>
2024-10-27 10:21:16 +0700

    remove unused failing eslint

--- dashboard/starter-example/app/ui/dashboard/latest-invoices.tsx
+++ dashboard/starter-example/app/ui/dashboard/latest-invoices.tsx
@@ -2,7 +2,7 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
+
 export default async function LatestInvoices({
   latestInvoices,
 }: {
--- dashboard/starter-example/app/ui/dashboard/revenue-chart.tsx
+++ dashboard/starter-example/app/ui/dashboard/revenue-chart.tsx
@@ -1,7 +1,6 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
 
 // This component is representational only.
 // For data visualization UI, check out:
