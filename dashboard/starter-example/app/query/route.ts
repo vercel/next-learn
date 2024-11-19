5d658298e9b9e5b896e65bce2bbea81e847f188c
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2024-11-19 10:45:50 +0000

    prettier-fix

--- dashboard/starter-example/app/query/route.ts
+++ dashboard/starter-example/app/query/route.ts
@@ -14,13 +14,13 @@
 // }
 
 export async function GET() {
-	return Response.json({
-		message:
-			"Uncomment this file and remove this line. You can delete this file when you are finished.",
-	});
-	// try {
-	// 	return Response.json(await listInvoices());
-	// } catch (error) {
-	// 	return Response.json({ error }, { status: 500 });
-	// }
+  return Response.json({
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  });
+  // try {
+  // 	return Response.json(await listInvoices());
+  // } catch (error) {
+  // 	return Response.json({ error }, { status: 500 });
+  // }
 }
