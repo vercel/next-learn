871ccb5d364327550e44d7f41e9ee94e43b8f152
Lee Robinson <lrobinson2011@gmail.com>
2024-12-05 15:03:05 -0600

    Fix

--- dashboard/final-example/app/lib/actions.ts
+++ dashboard/final-example/app/lib/actions.ts
@@ -108,15 +108,8 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  // throw new Error('Failed to Delete Invoice');
-
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice' };
-  } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
-  }
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
 
 export async function authenticate(
