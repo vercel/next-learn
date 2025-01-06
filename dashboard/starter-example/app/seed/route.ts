db63df1809554c2a8301d8f00337684fe48a6672
Tyler McRae <tyme@McBook-Bro.local>
2025-01-05 20:46:09 -0700

    added more detailed instructions to specify that the export function's initial return should be removed before seeding the database.

--- dashboard/starter-example/app/seed/route.ts
+++ dashboard/starter-example/app/seed/route.ts
@@ -104,7 +104,7 @@
 export async function GET() {
   return Response.json({
     message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+      'Uncomment all lines in this file and then remove the return Response.json that contains this message.  You can delete this file when you are finished.',
   });
   // try {
   //   await client.sql`BEGIN`;
