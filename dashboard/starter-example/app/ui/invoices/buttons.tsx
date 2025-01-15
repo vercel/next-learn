f83f04cf3253ec01ee39f93b144224425767bf14
Lee Robinson <lee@leerob.com>
2025-01-15 13:47:00 -0600

    Update buttons.tsx

--- dashboard/starter-example/app/ui/invoices/buttons.tsx
+++ dashboard/starter-example/app/ui/invoices/buttons.tsx
@@ -27,7 +27,7 @@ export function UpdateInvoice({ id }: { id: string }) {
 export function DeleteInvoice({ id }: { id: string }) {
   return (
     <>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
+      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
