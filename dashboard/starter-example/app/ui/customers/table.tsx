e7dd7e143ff66ed500e82c1b5666a09ab35621c5
Lee Robinson <lee@leerob.com>
2025-01-18 18:59:14 -0600

    Revert "remove unused failing eslint (#894)"
    This reverts commit 29f96d1fc1ffc9d0d6e2553bf0f11bcad70ee75d.

--- dashboard/starter-example/app/ui/customers/table.tsx
+++ dashboard/starter-example/app/ui/customers/table.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import { FormattedCustomersTable } from '@/app/lib/definitions';
+import {
+  CustomersTableType,
+  FormattedCustomersTable,
+} from '@/app/lib/definitions';
 
 export default async function CustomersTable({
   customers,
