fc6c41c8d136fdccd5db69005d1f46702d5cf633
Lee Robinson <lee@leerob.com>
2025-01-21 12:09:06 -0600

    Update auth chapter (#983)

--- dashboard/final-example/app/ui/dashboard/sidenav.tsx
+++ dashboard/final-example/app/ui/dashboard/sidenav.tsx
@@ -21,7 +21,7 @@ export default function SideNav() {
         <form
           action={async () => {
             'use server';
-            await signOut();
+            await signOut({ redirectTo: '/' });
           }}
         >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
