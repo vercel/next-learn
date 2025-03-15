1f09e760a30a09ad45542c8e391c48552c9922a9
Priyansh Shah <Priyanshpokemon@gmail.com>
2025-03-15 01:18:09 -0700

    fix: making auth also use bcryptjs

--- dashboard/final-example/auth.ts
+++ dashboard/final-example/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
