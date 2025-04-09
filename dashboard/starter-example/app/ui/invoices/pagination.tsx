8e2cdecc290f185424b7ff3c7bdce79447ea0856
Zackary <davididota@gmail.com>
2025-04-09 14:40:49 +0800

    fix: Add index to Pagination key for uniqueness in starter-example

--- dashboard/starter-example/app/ui/invoices/pagination.tsx
+++ dashboard/starter-example/app/ui/invoices/pagination.tsx
@@ -32,7 +32,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
             return (
               <PaginationNumber
-                key={page}
+                key={`${page}-${index}`}
                 href={createPageURL(page)}
                 page={page}
                 position={position}
