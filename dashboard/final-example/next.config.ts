1a3863a29bf0fa552dd4b0d844890e3d96eeffc5
Joseph Chamochumbi <joseph.chamochumbi@vercel.com>
2025-05-23 17:04:21 +0200

    chore: Prettier fixes

--- dashboard/final-example/next.config.ts
+++ dashboard/final-example/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    nodeMiddleware: true
-  }
+    nodeMiddleware: true,
+  },
 };
 
 export default nextConfig;
