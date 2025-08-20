9d37d4c38a08c3ed9289dfc54e5d3b6a9844fc0f
Joseph Chamochumbi <joseph.chamochumbi@vercel.com>
2025-08-20 15:24:10 +0200

    feat: nodejs runtime for middleware

--- dashboard/final-example/middleware.ts
+++ dashboard/final-example/middleware.ts
@@ -6,4 +6,5 @@ export default NextAuth(authConfig).auth;
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  runtime: 'nodejs',
 };
