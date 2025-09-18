1a38a510dcdb0a2be74a53e7bb45952a2469dbc0
Jurek Müller <jurek_mueller@yahoo.de>
2025-09-18 12:04:14 +0200

    Add intended peer focus color to customer select icon in invoice forms

--- dashboard/final-example/app/ui/invoices/edit-form.tsx
+++ dashboard/final-example/app/ui/invoices/edit-form.tsx
@@ -48,7 +48,7 @@ export default function EditInvoiceForm({
                 </option>
               ))}
             </select>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
           </div>
 
           <div id="customer-error" aria-live="polite" aria-atomic="true">
