a33d2d172330c1d12c0dfaaf244895112317ca6c
codomposer <akileus0902@gmail.com>
2025-10-29 10:11:18 -0400

    fixed missing a ; inside the { children: React.ReactNode; } in ./dashboard/final-example/app/dashboard/layout.tsx file

--- dashboard/final-example/app/dashboard/layout.tsx
+++ dashboard/final-example/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode; }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
